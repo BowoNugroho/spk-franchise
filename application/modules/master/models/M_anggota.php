381d595788a8b1e8ba21608579cf3223f80c3776
BowoNugroho <bowotkj10@gmail.com>
2023-05-27 00:54:00 +0700

    update

---
+++ application/modules/master/models/M_anggota.php
@@ -0,0 +1,53 @@
+<?php
+class M_anggota extends CI_Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+    var $sess;
+    public function list_data()
+    {
+        $sql = "SELECT a.* 
+        FROM anggota a ";
+        $query = $this->db->query($sql,);
+        return $query->result_array();
+    }
+
+    public function save_anggota()
+    {
+        $data = $this->input->post();
+        $date_now = date('Y-m-d');
+
+        $data['tgl_catat'] =  $date_now;
+        $data['anggota_nm'] =  strtoupper($data['anggota_nm']);
+        if ($data['anggota_id'] == '') {
+            $get_anggota = $this->db->where('tgl_catat', $date_now)->order_by('anggota_id', 'DESC')->get('anggota')->row_array();
+            if ($get_anggota == null) {
+                $data['anggota_id'] = date('ymd') . '0001';
+            } else {
+                $data['anggota_id'] = $get_anggota['anggota_id'] + 1;
+            }
+            $data['created_at'] = date('Y-m-d H:i:s');
+            $data['created_by'] = $this->session->userdata('username');
+            $this->db->insert('anggota', $data);
+        } else {
+            $data['updated_at'] = date('Y-m-d H:i:s');
+            $data['updated_by'] = $this->session->userdata('username');
+            $this->db->where('anggota_id', $data['anggota_id'])->update('anggota', $data);
+        }
+    }
+    public function getAnggotaById($id = '')
+    {
+        $sql = "SELECT a.* 
+                FROM anggota a 
+                WHERE a.anggota_id = $id ";
+        $query = $this->db->query($sql);
+        return $query->row_array();
+    }
+    public function deleteAnggotaById($id)
+    {
+        $this->db->where('anggota_id', $id);
+        $this->db->delete('anggota');
+    }
+}
