c6dc1e14e6a75b77cb0ac6003c74b748149376e3
Bowo Nugroho <bowotkj10@gmail.com>
2023-06-09 20:31:57 +0700

    init

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/sistem-ekas/sistem-ekas/';
+$config['base_url'] = 'http://localhost/spk-franchise/spk-franchise/';
 
 /*
 |--------------------------------------------------------------------------
@@ -384,7 +384,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ekas_session';
+$config['sess_cookie_name'] = 'spk_session';
 $config['sess_samesite'] = 'Lax';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = NULL;
