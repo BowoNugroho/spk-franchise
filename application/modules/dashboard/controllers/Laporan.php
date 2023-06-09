b341c7c321d6efb5e4b7a2b576429196c5e79dcf
Bowo Nugroho <bowotkj10@gmail.com>
2023-06-09 17:12:41 +0700

    update

--- application/modules/dashboard/controllers/Laporan.php
+++ application/modules/dashboard/controllers/Laporan.php
@@ -35,59 +35,10 @@ class Laporan extends MY_Controller
         }
         $this->render('laporan/index', $data);
     }
-    // public function search()
-    // {
-    //     $data = $this->input->post();
-    //     $data['cookie'] = $this->cookie;
-    //     $data['tgl1'] = $data['tgl1'];
-    //     $data['tgl2'] = $data['tgl2'];
-    //     $data['menu'] = $this->menu;
-    //     $data['kas'] = $this->m_laporan->list_data($this->cookie);
-    //     $data['jimpitan'] = $this->m_laporan->list_jimpitan();
-    //     $this->render('laporan/index', $data);
-    // }
-    public function saveKas()
-    {
-        $this->form_validation->set_rules('anggota_id', 'Nama Anggota', 'required');
-        $this->form_validation->set_rules('jumlah_transaksi', 'Jumlah Transaksi', 'required');
-        $this->form_validation->set_rules('petugas_catat_id', 'Petugas Catat', 'required');
-        $this->form_validation->set_rules('transaksi_id', 'Transaksi', 'required');
-        $this->form_validation->set_rules('tgl_catat', 'Tanggal Catat', 'required');
-        $this->form_validation->set_rules('keterangan', 'Keterangan', 'required');
-
-        if ($this->form_validation->run()) {
-            $data = [
-                'success' => 1,
-
-            ];
-            // save data
-            $this->m_laporan->save_kas();
-            // mengembalikan dalam bentuk json
-            echo json_encode($data);
-        } else {
-            // validasi 
-            $data = [
-                'error' => true,
-                'anggota_id_error' => form_error('anggota_id'),
-                'jumlah_transaksi_error' => form_error('jumlah_transaksi'),
-                'petugas_catat_id_error' => form_error('petugas_catat_id'),
-                'transaksi_id_error' => form_error('transaksi_id'),
-                'tgl_catat_error' => form_error('tgl_catat'),
-                'keterangan_error' => form_error('keterangan'),
-
-            ];
-            echo json_encode($data);
-        }
-    }
     public function cetak()
     {
-        $data = $this->input->post();
-        var_dump($this->cookie['search']['tgl1']);
-        die;
-        $tgl1 = $data['tgl1'];
-        $tgl2 = $data['tgl2'];
-        $kas = $this->m_laporan->list_data();
-        $jimpitan = $this->m_laporan->list_jimpitan();
+        $kas = $this->m_laporan->list_data($this->cookie);
+        $jimpitan = $this->m_laporan->list_jimpitan($this->cookie);
         $this->load->file(APPPATH . 'libraries/PHPExcel.php');
         $master_cetak = BASEPATH . 'master_cetak/laporan.xlsx';
         $PHPExcel = PHPExcel_IOFactory::load($master_cetak);
@@ -110,7 +61,7 @@ class Laporan extends MY_Controller
             ),
 
             'font' => array(
-                'size'  => 8,
+                'size'  => 12,
                 'name'  => 'Calibri'
             )
         );
@@ -202,22 +153,74 @@ class Laporan extends MY_Controller
                 ),
             ),
             'font' => array(
-                'size'  => 11,
+                'size'  => 12,
                 'name'  => 'Times New Roman'
             ),
         );
 
 
-        $PHPExcel->getActiveSheet()->setCellValue("A4", 'PERIODE TANGGAL ' . $tgl1 . ' s/d ' . $tgl2);
+        $PHPExcel->getActiveSheet()->setCellValue("A4", 'PERIODE TANGGAL ' . to_date($this->cookie['search']['tgl1'], '', 'date') . ' s/d ' . to_date($this->cookie['search']['tgl2'], '', 'date'));
+
+        // kas
         $no = 1;
         $i = 8;
+        $total_pemasukan_kas = 0;
+        $total_pengeluaran_kas = 0;
+        $netto_kas = 0;
+        asort($kas);
         foreach ($kas as $row) {
+            $total_pemasukan_kas += $row['jumlah_pemasukan'];
+            $total_pengeluaran_kas += $row['jumlah_pengeluaran'];
+            $netto_kas = $total_pemasukan_kas - $total_pengeluaran_kas;
             $PHPExcel->getActiveSheet()->setCellValue("A$i", $no++)->getStyle("A$i")->applyFromArray($align_center);
-            $PHPExcel->getActiveSheet()->setCellValue("B$i",  $row['per'])->getStyle("B$i")->applyFromArray($align_center);
-            $PHPExcel->getActiveSheet()->setCellValue("C$i",  num_id(@$row['jumlah_pemasukan']))->getStyle("C$i")->applyFromArray($align_center);
+            $PHPExcel->getActiveSheet()->setCellValue("B$i",  to_date($row['tgl_catat'], '', 'date'))->getStyle("B$i")->applyFromArray($align_center);
+            $PHPExcel->getActiveSheet()->setCellValue("C$i",  'Rp. ' . num_id(@$row['jumlah_pemasukan']))->getStyle("C$i")->applyFromArray($align_left);
+            $PHPExcel->getActiveSheet()->setCellValue("D$i",  'Rp. ' . num_id(@$row['jumlah_pengeluaran']))->getStyle("D$i")->applyFromArray($align_left);
+            $i++;
         }
-        $last_cell = "C" . ($i - 1);
+        $PHPExcel->getActiveSheet()->setCellValue("A$i",  'Total')->getStyle("A$i")->applyFromArray($align_center);
+        $PHPExcel->getActiveSheet()->setCellValue("C$i",  'Rp. ' . num_id(@$total_pemasukan_kas))->getStyle("C$i")->applyFromArray($align_left);
+        $PHPExcel->getActiveSheet()->setCellValue("D$i",  'Rp. ' . num_id(@$total_pengeluaran_kas))->getStyle("D$i")->applyFromArray($align_left);
+        $PHPExcel->getActiveSheet()->mergeCells("A$i:B$i")->getStyle("A$i:" . "B$i")->applyFromArray($align_center);
+        $i++;
+        $PHPExcel->getActiveSheet()->setCellValue("A$i",  'Jumlah Kas Bersih')->getStyle("A$i")->applyFromArray($align_center);
+        $PHPExcel->getActiveSheet()->mergeCells("A$i:B$i")->getStyle("A$i:" . "B$i")->applyFromArray($align_center);
+        $PHPExcel->getActiveSheet()->setCellValue("C$i",  'Rp. ' . num_id(@$netto_kas))->getStyle("C$i")->applyFromArray($align_center);
+        $PHPExcel->getActiveSheet()->mergeCells("C$i:D$i")->getStyle("C$i:" . "D$i")->applyFromArray($align_center);
+
+        // jimpitan
+        $noo = 1;
+        $ii = 8;
+        $total_pemasukan_jimpitan = 0;
+        $total_pengeluaran_jimpitan = 0;
+        $netto_jimpitan = 0;
+        asort($jimpitan);
+        foreach ($jimpitan as $row2) {
+            $total_pemasukan_jimpitan += $row2['jumlah_pemasukan'];
+            $total_pengeluaran_jimpitan += $row2['jumlah_pengeluaran'];
+            $netto_jimpitan = $total_pemasukan_jimpitan - $total_pengeluaran_jimpitan;
+            $PHPExcel->getActiveSheet()->setCellValue("F$ii", $noo++)->getStyle("F$ii")->applyFromArray($align_center);
+            $PHPExcel->getActiveSheet()->setCellValue("G$ii",  to_date($row2['tgl_catat'], '', 'date'))->getStyle("G$ii")->applyFromArray($align_center);
+            $PHPExcel->getActiveSheet()->setCellValue("H$ii",  'Rp. ' . num_id(@$row2['jumlah_pemasukan']))->getStyle("H$ii")->applyFromArray($align_left);
+            $PHPExcel->getActiveSheet()->setCellValue("I$ii",  'Rp. ' . num_id(@$row2['jumlah_pengeluaran']))->getStyle("I$ii")->applyFromArray($align_left);
+            $ii++;
+        }
+        $PHPExcel->getActiveSheet()->setCellValue("F$ii",  'Total')->getStyle("F$ii")->applyFromArray($align_center);
+        $PHPExcel->getActiveSheet()->setCellValue("H$ii",  'Rp. ' . num_id(@$total_pemasukan_jimpitan))->getStyle("H$ii")->applyFromArray($align_left);
+        $PHPExcel->getActiveSheet()->setCellValue("I$ii",  'Rp. ' . num_id(@$total_pengeluaran_jimpitan))->getStyle("I$ii")->applyFromArray($align_left);
+        $PHPExcel->getActiveSheet()->mergeCells("F$ii:G$ii")->getStyle("F$ii:" . "G$ii")->applyFromArray($align_center);
+        $ii++;
+        $PHPExcel->getActiveSheet()->setCellValue("F$ii",  'Jumlah Jimpitan Bersih')->getStyle("F$ii")->applyFromArray($align_center);
+        $PHPExcel->getActiveSheet()->mergeCells("F$ii:G$ii")->getStyle("F$ii:" . "G$ii")->applyFromArray($align_center);
+        $PHPExcel->getActiveSheet()->setCellValue("H$ii",  'Rp. ' . num_id(@$netto_jimpitan))->getStyle("H$ii")->applyFromArray($align_center);
+        $PHPExcel->getActiveSheet()->mergeCells("H$ii:I$ii")->getStyle("H$ii:" . "I$ii")->applyFromArray($align_center);
+
+        $last_cell = "D" . ($i - 1);
         $PHPExcel->getActiveSheet()->getStyle("A8:$last_cell")->applyFromArray($styleArray);
+
+        $last_cell2 = "I" . ($ii - 1);
+        $PHPExcel->getActiveSheet()->getStyle("F8:$last_cell2")->applyFromArray($styleArray);
+
 
 
         // Save it as file ------------------------------------------------------------------
--- application/modules/dashboard/views/laporan/index.php
+++ application/modules/dashboard/views/laporan/index.php
@@ -10,16 +10,20 @@
             <?= $this->session->flashdata('message'); ?>
             <span id="success_message"></span>
             <form id="form-search" action="<?= site_url() .  '/management/menu/search/' . $menu['menu_id']  ?>" method="post" autocomplete="off">
-                <div class="col-lg-3 col-md-3 mb-2">
-                    <input type="date" class="form-control " name="tgl1" id="tgl1" value="<?= @$cookie['search']['tgl1'] ?>">
-                </div>
-                <div class="col-lg-3 col-md-3 mb-2">
-                    <input type="date" class="form-control " name="tgl2" id="tgl2" value="<?= @$cookie['search']['tgl2'] ?>">
-                </div>
-                <div class="col-md-6 pl-2 mb-2">
-                    <button type="submit" class="btn btn-xs btn-primary" data-toggle="tooltip" data-placement="top" data-custom-class="tooltip-primary" title="" data-original-title="Tampilkan Data"><i class="fas fa-search"></i> Tampilkan</button>
-                    <a class="btn btn-xs  btn-warning" href="<?= site_url() . '/management/menu/reset/' . $menu['menu_id']  ?>" data-toggle="tooltip" data-placement="top" data-custom-class="tooltip-default" title="" data-original-title="Reset"><i class="fa fa-sync-alt"></i></a>
-                    <a class="btn btn-xs  btn-info" href="<?= site_url() . '/dashboard/laporan/cetak' ?>" data-toggle="tooltip" data-placement="top" data-custom-class="tooltip-default" title=""><i class="fa fa-print"> Cetak</i></a>
+                <div class="row">
+                    <label class="col-md-1">Tgl Awal</label>
+                    <div class="col-md-2 col-md-3 mb-2">
+                        <input type="date" class="form-control " name="tgl1" id="tgl1" value="<?= @$cookie['search']['tgl1'] ?>">
+                    </div>
+                    <label class="col-md-1">Tgl Akhir</label>
+                    <div class="col-md-2 col-md-3 mb-2">
+                        <input type="date" class="form-control " name="tgl2" id="tgl2" value="<?= @$cookie['search']['tgl2'] ?>">
+                    </div>
+                    <div class="col-md-3 col-md-3 mb-2">
+                        <button type="submit" class="btn btn-xs btn-primary" data-toggle="tooltip" data-placement="top" data-custom-class="tooltip-primary" title="" data-original-title="Tampilkan Data"><i class="fas fa-search"></i> Tampilkan</button>
+                        <a class="btn btn-xs  btn-warning" href="<?= site_url() . '/management/menu/reset/' . $menu['menu_id']  ?>" data-toggle="tooltip" data-placement="top" data-custom-class="tooltip-default" title="" data-original-title="Reset"><i class="fa fa-sync-alt"></i></a>
+                        <a class="btn btn-xs  btn-info" href="<?= site_url() . '/dashboard/laporan/cetak' ?>" data-toggle="tooltip" data-placement="top" data-custom-class="tooltip-default" title=""><i class="fa fa-print"> Cetak</i></a>
+                    </div>
                 </div>
             </form>
             <!-- <input type="text" class="form-control " name="tgl3" id="tgl3" value="<?= @$cookie['search']['tgl3'] ?>"> -->
